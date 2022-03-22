de3432c5f10e77a0e80caf2b86a62bb20d930647
Justin Hamerly <suvimountain@icloud.com>
2022-03-22 12:36:08 -0700

    games state now properly pulling from database on app load

--- src/actions/games.js
+++ src/actions/games.js
@@ -1,13 +1,15 @@
 import * as api from '../api';
 
+
 // action creators
 
 export const getGames = () => async (dispatch) => {
-
+  
   try{
 
     const { data } = await api.fetchGames();
-    dispatch({type: 'FETCH_ALL', data});
+    console.log(data);
+    dispatch({type: 'FETCH_ALL', payload: data});
 
   }catch(e){
 
--- src/components/Main/Games/Games.js
+++ src/components/Main/Games/Games.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { useSelector } from 'react-redux';
-import { Paper, Typography, Card, Button, Drawer } from '@material-ui/core';
+import { Typography, Card, Button, Drawer } from '@material-ui/core';
 
 function Games(props) {
   
@@ -8,25 +8,25 @@ function Games(props) {
   console.log(games);
 
   return (
-    <Paper>
+    <Fragment>
       <Typography variant="h4" align="center">Games</Typography>
       <Button onClick={() => props.setModalOpen(true)}>New Game</Button>
       <Button onClick={() => props.setDrawerOpen(true)}>My Games</Button>
       <Drawer open={props.drawerOpen} onClose={props.handleDrawerClose} >
         {
           !games.length ? <Typography>No Games</Typography> : (
-            <Paper>
-              {games.map(game => (
+            <Fragment>
+              {games[0].map(game => (
                 <Card key={game._id}>
                   <Typography>Players: {game.players.map(player => player.name + ' ') }</Typography>
                   <Typography>Terrain Types: {game.terrain.map(type => type + ' ')}</Typography>
                 </Card> 
               ))}
-            </Paper>
+            </Fragment>
           )
         }
       </Drawer>
-    </Paper>
+    </Fragment>
   )  
 }
 
