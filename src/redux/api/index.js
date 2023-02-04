e5176a1ee6b7a4d5e34d9bbc5757bb990f9dfc77
Justin Hamerly <suvimountain@icloud.com>
2023-02-03 17:12:49 -0800

    no longer needed or statement

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_SERVER || 'http://localhost:5000';
+const url = process.env.REACT_APP_SERVER;
 
 export const fetchGames = () => axios.get(`${url}/games`);
 export const createGame = (newGame) => axios.post(`${url}/games`, newGame);
